edd1d534d74a91c6830c22558213ad268fb34d86
nishimura lab PC 2 <nishimuratk@keio.jp>
2024-12-31 23:02:46 +0900

    画像変換機能を実装したが，サーバー側でのGUIを必要とする操作が原因でエラーが発生

--- backend/src/main/java/com/backend/backend/service/ImageProcessingService.java
+++ backend/src/main/java/com/backend/backend/service/ImageProcessingService.java
@@ -1,15 +1,50 @@
 package com.backend.backend.service;
 
-public class ImageProcessingService {
-    public static void main(String[] args){
-        System.out.println("test");
-        // ij.ImageJ ij = new ij.ImageJ();
+import java.nio.file.Paths;
 
-        // try {
-            
-        // } catch (Exception e) {
-        //     System.err.println("error: "+e.getMessage());
-        //     e.printStackTrace();
-        // }
-    } 
+import org.springframework.stereotype.Service;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.process.ImageProcessor;
+
+@Service
+public class ImageProcessingService {
+    private String getAbsolutePath(String relativePath){
+        // ImageJは実行フォルダを認識しないので，絶対パスに直して渡す
+        String executionPath = System.getProperty("user.dir");
+        String absolutePath = Paths.get(executionPath, relativePath).toString();
+        return absolutePath;
+    }
+
+    private String createOutputPath(String inputPath){
+        // 最後のピリオド(.)位置を取得し，その前に"_edited"を挿入
+        StringBuilder sb = new StringBuilder();
+        sb.append(inputPath);
+        int index = inputPath.lastIndexOf(".")-1;
+        sb.insert(index, "_edited");
+        return sb.toString();
+    }
+
+    public String convertColorToGrey(String inputPath){
+        new ij.ImageJ();
+        String outputPath = createOutputPath(inputPath);
+
+        try {
+            ImagePlus image = IJ.openImage(getAbsolutePath(inputPath));
+            if(image == null){
+                throw new Exception("画像が読み込めません："+inputPath);
+            }
+
+            ImageProcessor processor =  image.getProcessor();
+            processor = processor.convertToByte(true);
+
+            ImagePlus editedImage = new ImagePlus("Edited Image", processor);
+            IJ.save(editedImage, outputPath);
+        } catch (Exception e) {
+            System.err.println("error: "+e.getMessage());
+            e.printStackTrace();
+        }
+        return outputPath;
+    }
 }
